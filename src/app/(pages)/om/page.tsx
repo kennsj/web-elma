c8cf51bb800112b7088c29a98ffd173514444a4d
kennsj <kennethsjorgensen@gmail.com>
2025-07-02 13:15:49 +0200

    Dummy content to pages

---
+++ src/app/(pages)/om/page.tsx
@@ -0,0 +1,15 @@
+import { Hero } from "@/components/Header/Hero"
+import React from "react"
+
+const page = () => {
+	return (
+		<>
+			<Hero />
+			<section>
+				<h1>Hei</h1>
+			</section>
+		</>
+	)
+}
+
+export default page
