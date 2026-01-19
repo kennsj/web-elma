f7e2b4abac7cb1b8a93a8ca2e8b19e1944e5f958
Kenneth Jørgensen <kennethsjorgensen@gmail.com>
2026-01-19 15:19:12 +0100

    Aaaaaaaand one last time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import WaveCss from "@/components/Layout/UI/WaveSeperator/WaveSeperator"
 // import EventList from "@/components/Lists/EventList/EventList"
 import About from "@/components/Layout/Cards/Card/FeaturedCard"
 import { TextNarrow, TextWide } from "@/components/ui/TextBlocks"
-import { FadeInLetters } from "@/components/Layout/UI/Animations/ParagraphFadeIn"
 import Image from "next/image"
 import Anchor from "@/components/Buttons/Anchor"
 
